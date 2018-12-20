e3fc516e092fe45174e9bf41fc944d572172cd2c
Edem <keltikan@gmail.com>
2018-12-20 14:54:06 +0300

    Code refactoring

--- lib/film_collection.rb
+++ lib/film_collection.rb
@@ -1,56 +1,41 @@
 class FilmCollection
-  attr_accessor :films_library, :offer
+  attr_accessor :library, :offer
   attr_reader :directors_list
 
   def initialize(dir_path)
     begin
-      files = Dir.entries(dir_path)
-
       # В папке могут быть другие файлы, например *.avi
       # Кроме того, в ряду 0.txt .. 5.txt может, например, отсутствовать 3.txt
       # Будем считать, что все файлы типа 2.txt и 235.txt содержат-таки фильмы
-      films_files = []
-      files.each do |value|
-        films_files << value if value =~ /^\d+.txt/
-      end
+      films_files = Dir.entries(dir_path).select { |value| value =~ /^\d+.txt/ }
 
-      @films_library = []
-      films_files.each do |value|
+      @library = films_files.map do |value|
         f = File.new(dir_path + value)
         lines = f.readlines
         f.close
-        @films_library << { id:      value.delete(".txt").to_i,
-                           title:    lines[0].chomp,
-                           director: lines[1].chomp,
-                           year:     lines[2].chomp }
+        { id:       value.delete('.txt').to_i,
+          title:    lines[0].chomp,
+          director: lines[1].chomp,
+          year:     lines[2].chomp }
       end
+      puts @library
     rescue SystemCallError
-      @films_library = []
+      @library = []
     end
   end
 
   def set_directors_list
-    @directors_list = []
-
-    @films_library.each { |value| @directors_list << value[:director] }
+    @directors_list = @library.map { |value| value[:director] }
 
     @directors_list.uniq!
   end
 
   def print_directors_list
-    print_directors_list = []
-
-    @directors_list.each_with_index { |value, index| print_directors_list <<
-      "#{index + 1}. #{value}" }
-
-    print_directors_list
+    @directors_list.map.with_index { |value, index| "#{index + 1}. #{value}" }
   end
 
   def offer_film(user_choose)
-    choosing_films = []
-    @films_library.each do |value|
-      choosing_films << value if value[:director] == @directors_list[user_choose]
-    end
+    choosing_films = @library.select { |value| value[:director] == @directors_list[user_choose] }
     @offer = choosing_films.sample
   end
 end
--- main.rb
+++ main.rb
@@ -1,24 +1,24 @@
 require_relative 'lib/film_collection.rb'
 
 current_path = File.dirname(__FILE__)
-seans = FilmCollection.new(current_path + "/data/")
-abort "Сегодня фильмов нет" if seans.films_library.empty?
+collection = FilmCollection.new(current_path + '/data/')
+abort 'Сегодня фильмов нет' if collection.library.empty?
 
-STDOUT.puts "Фильмы какого режиссера Вы хотите посмотреть?"
+STDOUT.puts 'Фильмы какого режиссера Вы хотите посмотреть?'
 
-seans.set_directors_list
+collection.set_directors_list
 
-STDOUT.puts seans.print_directors_list
+STDOUT.puts collection.print_directors_list
 
 user_choose = nil
-until ("1".."#{seans.directors_list.length}").include? user_choose
-  STDOUT.puts "Введите цифру, соответствующую режиссёру"
+until ("1".."#{collection.directors_list.length}").include? user_choose
+  STDOUT.puts 'Введите цифру, соответствующую режиссёру'
   user_choose = STDIN.gets.chomp
 end
 user_choose = user_choose.to_i - 1
 
-seans.offer_film(user_choose)
+collection.offer_film(user_choose)
 
-STDOUT.puts "Рекомендуем посмотреть:"
-STDOUT.puts "#{seans.offer[:director]} — #{seans.offer[:title]} " \
-  "(#{seans.offer[:year]})"
+STDOUT.puts 'Рекомендуем посмотреть:'
+STDOUT.puts "#{collection.offer[:director]} — #{collection.offer[:title]} " \
+  "(#{collection.offer[:year]})"
